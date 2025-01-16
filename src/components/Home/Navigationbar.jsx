95706502f277d8df1ca33f9842fe3f66f144afa1
Mohammed Mustaqeem <mustaqeemcode@gmail.com>
2025-01-16 23:27:37 +0530

    ten commit

--- src/components/Home/Navigationbar.jsx
+++ src/components/Home/Navigationbar.jsx
@@ -9,13 +9,7 @@ import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Cookies from "js-cookie";
 import { handleSuccess } from "../../assets/utils";
-import { ToastContainer, Bounce } from "react-toastify";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import {
-//   faFacebook,
-//   faTwitter,
-//   faInstagram,
-// } from "@fortawesome/free-brands-svg-icons"; 
+import { ToastContainer, Bounce } from "react-toastify"; 
 import './Navigationbar.css'
 
 const Navigationbr = () => {
@@ -47,200 +41,6 @@ const Navigationbr = () => {
 
   return (
     <>
-      {/* <div className="header-top d-none d-sm-block">
-        <div className="container">
-          <div className="row">
-            <div className="col-xl-12">
-              <div className="d-flex justify-content-between flex-wrap align-items-center">
-                
-                <div className="header-info-left ">
-                  <ul className="flex gap-5 text-[14px]">
-                    <li>
-                      <Link
-                        className="no-underline text-gray-900"
-                        to="/about-us"
-                      >
-                        About Us
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        className="no-underline text-gray-900"
-                        to="/privacy"
-                      >
-                        Privacy
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="no-underline text-gray-900" to="/faq">
-                        FAQ
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        className="no-underline text-gray-900"
-                        to="/careers"
-                      >
-                        Careers
-                      </Link>
-                    </li>
-                  </ul>
-                </div>
-
-                
-                <div className="  header-info-right d-flex">
-                  
-                  <ul className="order-list flex gap-4 text-[14px]">
-                    <li>
-                      <Link
-                        className="no-underline text-gray-900"
-                        to="/wishlist"
-                      >
-                        My Wishlist
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        className="no-underline text-gray-900"
-                        to="/track-order"
-                      >
-                        Track Your Order
-                      </Link>
-                    </li>
-                  </ul>
-
-                  <ul className="header-social flex ml-3 gap-2 ">
-                    <li>
-                      <a
-                        className="no-underline  "
-                        href="https://facebook.com"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <i className="fa fa-facebook"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        className="no-underline "
-                        href="https://instagram.com"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <i className="fa fa-instagram"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        className="no-underline "
-                        href="https://twitter.com"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <i className="fa fa-twitter"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        className="no-underline text-gray-500"
-                        href="https://linkedin.com"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <i className="fa fa-linkedin-in"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        className="no-underline "
-                        href="https://youtube.com"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <i className="fa fa-youtube"></i>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <Navbar className="">
-        <Container
-          fluid
-          className="p-3  navbar-expand-lg navbar-light bg-light container"
-        >
-          <Navbar.Brand href="/" className="fw-bold ">
-            Quick<span style={{ color: "red" }}>Mart</span>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0  flex space-x-8 "
-              style={{ maxHeight: "200px" }}
-              navbarScroll
-            >
-              <NavLink className="no-underline text-gray-500" to="/">
-                Home
-              </NavLink>
-              <NavLink className="no-underline text-gray-500" to="/products">
-                Products
-              </NavLink>
-              <NavLink className="no-underline text-gray-500" to="/About">
-                About
-              </NavLink>
-              <NavLink className="no-underline text-gray-500" to="/Contact">
-                Contact
-              </NavLink>
-            </Nav>
-
-            <div className="button">
-              {isLoggedIn ? (
-                <Button
-                  variant="outline-dark"
-                  className="fa fa-sign-in ms-2"
-                  onClick={handleLogout}
-                >
-                  {" "}
-                  Logout
-                </Button>
-              ) : (
-                <Button
-                  variant="outline-dark"
-                  className="fa fa-sign-in ms-2"
-                  onClick={handleLogin}
-                >
-                  {" "}
-                  Login
-                </Button>
-              )}
-              <Button
-                variant="outline-dark"
-                className="fa fa-shopping-cart ms-2"
-                onClick={() => handleCart()}
-              >
-                Cart ({state.length})
-              </Button>
-            </div>
-          </Navbar.Collapse>
-        </Container>
-        <ToastContainer
-          position="top-left"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="dark"
-          transition={Bounce}
-        />
-      </Navbar> */}
-
       <header>
         {/* Top Bar */}
         <div className="header-top bg-light py-2">
@@ -317,6 +117,10 @@ const Navigationbr = () => {
             </div>
           </div>
         </div>
+
+
+
+        
 
         {/* Navbar */}
         <Navbar expand="lg" bg="light" variant="light" sticky="top">
